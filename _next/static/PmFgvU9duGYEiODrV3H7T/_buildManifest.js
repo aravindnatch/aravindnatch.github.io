7e0b0973868ca549e5065b885e01c0f0f8bc8bbc
aravindnatch <aravindnatch@users.noreply.github.com>
2022-01-14 22:34:07 +0000

    deploy: 90b7e72fd131c469022bc83d1d55fe70daf2d503

--- _next/static/PmFgvU9duGYEiODrV3H7T/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-cbc668d5ae98b596.js","static/chunks/pages/index-b0e14489b63be8a3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

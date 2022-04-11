a1696564a8e3fe777bac3f21d89243d45aea2582
Aravind Natchiappan <anatchiappan3@gatech.edu>
2022-04-11 14:41:06 -0400

    updated mobile page

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from 'next'
+import Link from 'next/link';
 
 import links from '../data/links.json';
 
 import Head from 'next/head'
-import Image from 'next/image'
+
+import {
+  FaTiktok,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Mobile: NextPage = () => {
-  var sections = ["tiktok", "instagram", "twitter", "youtube", "twitch", "reddit"]
-  var items = links;
   return (
-    <div className="flex min-h-screen flex-col items-center pt-10 bg-black">
+    <div className="flex h-screen flex-col items-center pt-10 bg-black mb-auto">
       <Head>
         <title>Aravind Natchiappan</title>
         <link rel="icon" href="/favicon.ico" />
@@ -22,18 +26,29 @@ const Mobile: NextPage = () => {
       <br/>
 
       <div>
-        <h1 className="text-4xl font-semibold text-center text-white">Aravind Natchiappan</h1>
-        <h2 className="text-xl font-semibold text-center text-white">@aravindnatch</h2>
+        <h1 className="text-3xl font-semibold text-center text-white">Aravind Natchiappan</h1>
+        <h2 className="text-lg font-semibold text-center text-white">@aravindnatch</h2>
       </div>
 
+      <div className="flex mt-5">
+        <Link href="https://instagram.com/aravindnatch" passHref={true}>
+          <FaInstagram className="text-white text-2xl inline mx-4 cursor-pointer" />
+        </Link>
+        <Link href="https://www.tiktok.com/@aravindnatch" passHref={true}>
+          <FaTiktok className="text-white text-2xl inline mx-4 cursor-pointer" />
+        </Link>
+        <Link href="https://linkedin.com/in/aravindnatch/" passHref={true}>
+          <FaLinkedin className="text-white text-2xl inline mx-4 cursor-pointer" />
+        </Link>
+      </div>
 
       <div className="container max-w-2xl mt-5 p-4">
         {
-          items.map((section) => 
-            <a href={section['link']} target="_blank">
+          links.map((items) => 
+            <a href={items['link']} target="_blank" key={items['name']}>
               <div className="rounded-3xl w-full border-2 border-black p-5 mb-5 bg-white">
-                  <img src={section['icon']} className="rounded-xl w-10 h-10 inline mr-5"/>
-                  <h1 className="right text-center items-left inline">{section['name']}</h1>
+                  <img src={items['icon']} className="rounded-xl w-10 h-10 inline mr-5"/>
+                  <h1 className="right text-center items-left inline">{items['name']}</h1>
               </div>
             </a>
           )

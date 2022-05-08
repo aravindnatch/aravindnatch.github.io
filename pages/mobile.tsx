e88446bb788d09f0b7c630c0a01f77cb52eb04bc
Aravind Natchiappan <aravind.natch@gmail.com>
2022-05-08 03:30:51 -0400

    redesigned mobile page

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import links from '../data/links.json';
 import Head from 'next/head'
 
 import {
@@ -12,53 +11,82 @@ import {
 
 const Mobile: NextPage = () => {
   return (
-    <div className="flex h-screen flex-col items-center pt-10 bg-black mb-auto">
+    <div className="flex h-screen flex-col items-center bg-[#040608] mb-">
       <Head>
-        <title>Aravind Natchiappan</title>
+        <title>aravind natchiappan</title>
         <link rel="icon" href="/favicon.ico" />
-        <script defer data-domain="aravindnatch.me" src="https://analytics.aru.wtf/js/script.js"></script>
+        {/* <script defer data-domain="aravindnatch.me" src="https://analytics.aru.wtf/js/script.js"></script> */}
       </Head>
 
-      <div className="rounded-full border-2 border-black w-36 h-36">
-        <img src="/aravind.jpg" alt="logo"  width="100%" height="100%" className="rounded-full"/>
+      <div className="bg-[#111315] px-10 pt-10 pb-5 w-full">
+        <div className="flex">
+          <div className="w-32 h-32 min-w-32 min-h-32">
+            <img src="/aravind.jpg" alt="logo" width="100%" height="100%" className="rounded-full"/>
+          </div>
+
+          <div className="w-max flex-grow ml-6">
+            <h1 className="text-2xl font-semibold text-left text-white font">aravind</h1>
+
+            <div className="mt-4">
+              <h2 className="text-md text-left text-white">20 • photography</h2>
+              <h2 className="text-md text-left text-white">comp sci @ gatech</h2>
+              <h2 className="text-md text-left text-white">atlanta, georgia</h2>
+            </div>
+          </div>
+        </div>
+    
+        <div className="flex mt-7 justify-evenly">
+          <a href="mailto:hi@aravindnatch.me" target="_blank">
+            <FaEnvelope className="text-white text-2xl inline cursor-pointer" />
+          </a>
+          <a href="https://open.spotify.com/user/aravind.natch" target="_blank">
+            <FaSpotify className="text-white text-2xl inline cursor-pointer" />
+          </a>
+          <a href="https://instagram.com/aravindnatch" target="_blank">
+            <FaInstagram className="text-white text-2xl inline cursor-pointer" />
+          </a>
+          <a href="https://www.tiktok.com/@aravindnatch" target="_blank">
+            <FaTiktok className="text-white text-2xl inline cursor-pointer" />
+          </a>
+          <a href="https://linkedin.com/in/aravindnatch/" target="_blank">
+            <FaLinkedin className="text-white text-2xl inline cursor-pointer" />
+          </a>
+        </div>
       </div>
 
-      <br/>
-
-      <div>
-        <h1 className="text-3xl font-semibold text-center text-white">Aravind Natchiappan</h1>
-        <h2 className="text-lg font-semibold text-center text-white">@aravindnatch</h2>
-      </div>
-
-      <div className="flex mt-5">
-        <a href="mailto:hi@aravindnatch.me" target="_blank">
-          <FaEnvelope className="text-white text-2xl inline mx-4 cursor-pointer" />
-        </a>
-        <a href="https://open.spotify.com/user/aravind.natch" target="_blank">
-          <FaSpotify className="text-white text-2xl inline mx-4 cursor-pointer" />
-        </a>
-        <a href="https://instagram.com/aravindnatch" target="_blank">
-          <FaInstagram className="text-white text-2xl inline mx-4 cursor-pointer" />
-        </a>
-        <a href="https://www.tiktok.com/@aravindnatch" target="_blank">
-          <FaTiktok className="text-white text-2xl inline mx-4 cursor-pointer" />
-        </a>
-        <a href="https://linkedin.com/in/aravindnatch/" target="_blank">
-          <FaLinkedin className="text-white text-2xl inline mx-4 cursor-pointer" />
-        </a>
-      </div>
+      <div className="bg-[#111315] w-full rounded-b-3xl p-2"></div>
 
       <div className="container max-w-2xl mt-5 p-4">
-        {
-          links.map((items) => 
-            <a href={items['link']} target="_blank" key={items['name']}>
-              <div className="rounded-3xl w-full border-2 border-black p-5 mb-5 bg-white">
-                  <img src={items['icon']} className="rounded-xl w-10 h-10 inline mr-5"/>
-                  <h1 className="right text-center items-left inline">{items['name']}</h1>
-              </div>
-            </a>
-          )
-        }
+        <div className="w-full pb-5">
+          <h2 className="text-white text-left font-semibold">my games</h2>
+        </div>
+
+        <a href="https://tod.aru.wtf" target="_blank">
+          <div className="rounded-xl w-full border-2 border-white p-3 mb-5 bg-white">
+              <img src="/icons/tod.png" className="rounded-xl w-10 h-10 inline mr-5"/>
+              <h1 className="right text-center text-black items-left inline">truth or drink</h1>
+          </div>
+        </a>
+
+        <a href="https://aravindnatch.me/mobile" target="_blank">
+          <div className="rounded-xl w-full border-2 border-white p-3 mb-5 bg-white">
+              <img src="/icons/trivia.png" className="rounded-xl w-10 h-10 inline mr-5"/>
+              <h1 className="right text-center items-left inline">trivia cracked</h1>
+          </div>
+        </a>
+        
+        <hr className="opacity-20"/>
+
+        <div className="w-full py-5">
+          <h2 className="text-white text-left font-semibold">other links</h2>
+        </div>
+        
+        <a href="https://venmo.com/@aravindnatch?txn=pay&note=<3" target="_blank">
+          <div className="rounded-xl w-full border-2 border-white p-3 mb-5 bg-white">
+              <img src="/icons/venmo.png" className="rounded-xl w-10 h-10 inline mr-5"/>
+              <h1 className="right text-center items-left inline">venmo me</h1>
+          </div>
+        </a>
       </div>
     </div>
   )

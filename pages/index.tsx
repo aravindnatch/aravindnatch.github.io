807e13f84618c430a1c3ca58f28caa9bfd40b595
Aravind Natchiappan <aravind.natch@gmail.com>
2022-07-19 22:19:29 -0700

    index formatting change

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ const Home: NextPage = () => {
       <Head>
         <script defer src="https://omni.aru.wtf/script.js"></script>
       </Head>
-    <Fragment>
-      <MediaQuery minDeviceWidth={1224}>
-          <HomeComponent />
-      </MediaQuery>
-      <MediaQuery maxDeviceWidth={1224}>
-          <MobileComponent />
-      </MediaQuery>
-    </Fragment>
+      <Fragment>
+        <MediaQuery minDeviceWidth={1224}>
+            <HomeComponent />
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={1224}>
+            <MobileComponent />
+        </MediaQuery>
+      </Fragment>
     </>
   )
 }

7d4d36ea9a5e15a9118c2a7decbc579323d76a4f
Aravind Natchiappan <aravind.natch@gmail.com>
2022-09-12 16:24:16 -0400

    add analytics to forced mobile page

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import MobileComponent from '../components/mobile';
 
 const Mobile: NextPage = () => {
-  return <MobileComponent />
+  return (
+    <>
+      <Head>
+        <script async src="https://omni.aru.wtf/script.js"></script>
+      </Head>
+      <MobileComponent />
+    </>
+  )
 }
 
 export default Mobile
